544926ea0035e7d1b10b7991a02003acfb767935
江鑫 <jiangxin@jiangxindeMacBook-Air.local>
2018-04-12 16:05:47 +0800

    ff

---
+++ XJModuleProjectSDK/XJModuleConfigManager.h
@@ -0,0 +1,13 @@
+//
+//  XJModuleConfigManager.h
+//  XJModuleProjectSDK
+//
+//  Created by 江鑫 on 2018/4/12.
+//  Copyright © 2018年 XJ. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface XJModuleConfigManager : NSObject
+
+@end
---
+++ XJModuleProjectSDK/XJModuleConfigManager.m
@@ -0,0 +1,13 @@
+//
+//  XJModuleConfigManager.m
+//  XJModuleProjectSDK
+//
+//  Created by 江鑫 on 2018/4/12.
+//  Copyright © 2018年 XJ. All rights reserved.
+//
+
+#import "XJModuleConfigManager.h"
+
+@implementation XJModuleConfigManager
+
+@end
